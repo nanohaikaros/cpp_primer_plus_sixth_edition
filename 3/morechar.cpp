171c5e50ddc543a48946d9b83f46befed2c7b027
Nanoha <1371689491@qq.com>
2019-05-31 19:50:20 +0800

    3

--- 3/morechar.cpp
+++ 3/morechar.cpp
@@ -4,10 +4,13 @@ int main() {
     using namespace std;
     char ch = 'M';
     int i = ch;
+    cout << "The ADCII code for " << ch << " is " << i << endl;
+
     cout << "Add one to the character code:" << endl;
     ch = ch + 1;
     i = ch;
     cout << "The ADCII code for " << ch << " is " << i << endl;
+    
     cout << "Displaying char ch using cout.put(ch): ";
     cout.put(ch);
 
