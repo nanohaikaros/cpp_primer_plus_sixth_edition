1087cb16d34b00824e6d7b6c9beb0a0d197cb3c4
Nanoha <1371689491@qq.com>
2024-02-21 19:36:43 +0800

    Update arraynew.cpp

--- 4/arraynew.cpp
+++ 4/arraynew.cpp
@@ -5,7 +5,7 @@ int main()
     using namespace std;
     double *p3 = new double[3]; //space for 3 doubles
     p3[0] = 0.2;                // treat p3 like an array name
-    p2[1] = 0.5;
+    p3[1] = 0.5;
     p3[2] = 0.8;
     cout << "p3[1] is " << p3[1] << ".\n";
     p3 = p3 + 1;                // increment the pointer
